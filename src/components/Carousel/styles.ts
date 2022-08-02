f07c6dd72f1aea0cec94acdb9bbf345663b232e8
Afonso <afonsobezerra199@gmail.com>
2022-08-02 20:15:03 -0300

    fix image size cover

--- src/components/Carousel/styles.ts
+++ src/components/Carousel/styles.ts
@@ -4,23 +4,28 @@ export const ContainerCSS = styled.div`
   user-select: none;
   display: none;
 
+  height: 100%;
   max-width: 100%;
   max-height: 100vh;
 
   .embla {
     overflow: hidden;
     max-width: 100%;
+    height: 100%;
   }
   .embla__container {
     display: flex;
+    height: 100%;
   }
   .embla__slide {
     position: relative;
     overflow: hidden;
+    height: 100%;
     flex: 0 0 100%;
     img {
       width: 100%;
       height: 100%;
+      object-fit: cover;
     }
   }
 
